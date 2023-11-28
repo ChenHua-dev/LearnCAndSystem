2801b30c1e7ed85de214f9abcba459a5f2e61898
Chen Hua <chua0221@gmail.com>
2023-11-28 13:59:53 -0800

    change to ex13.c

--- ex13/ex13.c
+++ ex13/ex13.c
@@ -8,6 +8,7 @@ int main(int argc, char* argv[]) {
     }
 
     char* states[] = {"California", "Oregon", "Washington", "Texas"};
+    // char states[] = {'C', 'O', 'W', 'T'};
     int num_states = sizeof(states) / sizeof(states[0]);
     printf("Total size of states: %ld\n", sizeof(states));
     printf("Each element is of size: %ld %ld %ld %ld\n", sizeof(states[0]), sizeof(states[1]), sizeof(states[2]), sizeof(states[3]));
@@ -15,6 +16,12 @@ int main(int argc, char* argv[]) {
 
     for (i = 0; i < num_states; i++) {
         printf("%s \n", states[i]);
+        // printf("%c \n", states[i]);
+        char* ptr = states[i];
+        while (*ptr != '\0') {
+            printf("%c ", *(ptr++));
+        }
+        printf("\n");
     }
     
     return 0;
